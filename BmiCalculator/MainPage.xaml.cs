22e82fdf7f75fc291195c0db33ca217396f532de
karoluciecha <65387089+karoluciecha@users.noreply.github.com>
2025-09-29 01:44:54 +0100

    Add project files.

---
+++ BmiCalculator/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace BmiCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double height = (Convert.ToDouble(HeightEntry.Text)) / 100;
+                double weight = Convert.ToDouble(WeightEntry.Text);
+                double bmi = weight / (height * height);
+
+                lblAnswer.Text = "Your BMI is: " + bmi.ToString("F2");
+            }
+            catch (Exception)
+            {
+                lblAnswer.Text = "Please enter valid numbers for height and weight";
+            }
+        }
+    }
+}
